c262f71a2589211f91cca43a332a48d056136209
DrewMcArthur <DrewMcArthur1@gmail.com>
2022-04-28 00:03:49 -0400

    initialize some go files

---
+++ game/wordle.go
@@ -0,0 +1,146 @@
+/**
+ * main file defining the game of wordle
+ */
+
+package wordle
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/chzyer/readline"
+)
+
+func game() {
+	var (
+		acceptedGuesses string[] = getAcceptedGuesses()
+		possibleSolutions string[] = getPossibleSolutions()
+		secret string = getWord()
+		count  int    = 1
+		end    bool   = true
+	)
+	for end {
+		guess := getInput()
+		checkLetters(secret, guess)
+		if secret == guess {
+			fmt.Println("\033[32m" + "You won!" + "\033[0m")
+			end = false
+		}
+		if count == 5 && end {
+			fmt.Println("\033[31m" + "You lost!" + "\033[0m")
+			fmt.Println("The word was:", secret)
+			end = false
+		}
+		count++
+	}
+}
+
+func getAcceptedGuesses() {
+	return readFileToStringList('game/acceptedGuesses.txt"')
+}
+
+func getPossibleSolutions() {
+	return readFileToStringList('game/possibleSolutions.txt"')
+}
+
+func readFileToStringList(filename string) {
+	// TODO:
+	return []
+}
+
+// return all the 5 length words from file
+func getWordsFile(f *os.File) []string {
+	var scanner = bufio.NewScanner(f)
+	var words []string
+	for scanner.Scan() {
+		word := scanner.Text()
+		if len(word) == 5 {
+			words = append(words, word)
+		}
+	}
+	return words
+}
+
+// return a random 5 letters word from file
+func randomWordFile(filename string) string {
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	words := getWordsFile(f)
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(words))
+	return words[i]
+}
+
+// return a random word from the words list
+func randomWord() string {
+	// random word from list
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(words))
+	return words[i]
+}
+
+// get the word to guess during the game
+func getWord() string {
+	var word string
+	if len(os.Args) > 1 {
+		word = randomWordFile(os.Args[1])
+	} else {
+		word = randomWord()
+	}
+	return word
+}
+
+// compare letters between user input
+// and the guess word
+func checkLetters(secret, guess string) {
+	for i := 0; i < len(secret); i++ {
+		if secret[i] == guess[i] { // right place
+			fmt.Print("\033[32m", string(guess[i]), "\033[0m")
+		} else if strings.ContainsAny(secret, string(guess[i])) { // wrong place
+			fmt.Print("\033[34m", string(guess[i]), "\033[0m")
+		} else { // incorrect
+			fmt.Print("\033[31m", string(guess[i]), "\033[0m")
+		}
+	}
+	fmt.Println()
+}
+
+// check if input contains spaces
+// or if input is more/less than 5 characters long
+func checkInput(input string) bool {
+	if len(input) != 5 {
+		fmt.Println("Please enter 5 letters")
+		return false
+	} else if strings.ContainsAny(input, " ") {
+		fmt.Println("No space in words please..")
+		return false
+	}
+	return true
+}
+
+// get user input for the guess
+func getInput() string {
+	reader, err := readline.New("> ")
+	if err != nil {
+		return getInput()
+	}
+	input, err := reader.Readline()
+	if err != nil {
+		return getInput()
+	}
+	// to quit type exit
+	if string(input) == "exit" {
+		os.Exit(0)
+	}
+	if !checkInput(string(input)) {
+		return getInput()
+	}
+	return string(input)
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+/**
+ * This file is the main entry point of the repo, kicking off a game of wordle.
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to drew's implementation of wordle!")
+}
